53dbc590d577403b04c22b696cc91254238685d6
SemkaTsocurenco <semka0tsocurenco@gmail.com>
2025-03-06 09:07:22 +0300

    SFML add

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include "main.hpp"
 
 int main (){
-	std::cout<<"Hello\n";
+
+    sf::RenderWindow window(sf::VideoMode(500, 500), "TagGame", sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(30); 
+	sf::View view(sf::FloatRect(0, 0, 500, 500));
+    window.setView(view);
+    window.setVerticalSyncEnabled(true); 
+
+	  while (window.isOpen())
+    {
+		window.clear(sf::Color::White);
+		window.setView(view);
+		window.display();
+         
+    }
+	return 0;
 }
---
+++ main.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <cmath>  
+#include <SFML/System.hpp>
+#include <vector>
+#include <random>
