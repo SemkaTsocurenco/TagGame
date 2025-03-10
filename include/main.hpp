ac7378e8f57b90fb4a49e3129b9e326ac2881cc0
Semen Tsokurenko <semka0tsocurenco@gmail.com>
2025-03-10 08:34:26 +0300

    Проведён не конечный рефакторинг

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <stack>
 #include <cmath>  
 #include <SFML/System.hpp>
 #include <vector>
 #include <random>
+#include "tags.hpp"
+#include "Drag.hpp"
+#include "field.h"
+
